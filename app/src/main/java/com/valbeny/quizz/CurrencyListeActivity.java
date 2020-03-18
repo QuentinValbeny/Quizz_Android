9f2ca89476a3e3e9d72d34be72eefcd4430913b0
quentin <quentin.valbeny@gmail.com>
2020-03-18 16:21:44 +0100

    Création de la liste de question partie 1

---
+++ app/src/main/java/com/valbeny/quizz/CurrencyListeActivity.java
@@ -0,0 +1,14 @@
+package com.valbeny.quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrencyListeActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_currency_liste);
+	}
+}
