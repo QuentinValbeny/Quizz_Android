eb6c961b8c85473123db084636b186291373d5b3
quentin <quentin.valbeny@gmail.com>
2020-03-18 14:28:03 +0100

    Préparation des activitys about et quizz

---
+++ app/src/main/java/com/valbeny/quizz/MainActivity.java
@@ -0,0 +1,39 @@
+package com.valbeny.quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Log.i("MainActivity","Quizz lancé");
+
+		Button goQuizzButton = findViewById(R.id.goQuizzButton);
+		goQuizzButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, QuizzActivity.class); //Changer de view
+				startActivity(intent);//use intent
+			}
+		});
+
+		Button aboutButton = findViewById(R.id.aboutButton);
+		aboutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, aboutActivity.class); //Changer de view
+				startActivity(intent);//use intent
+			}
+		});
+	}
+}
---
+++ app/src/main/java/com/valbeny/quizz/QuizzActivity.java
@@ -0,0 +1,14 @@
+package com.valbeny.quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizzActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quizz);
+	}
+}
